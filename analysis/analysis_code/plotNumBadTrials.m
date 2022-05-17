9c55ba888257ea265bd169d5229aefea44211f14
khenheller <khenheller@mail.tau.ac.il>
2022-05-17 17:49:26 +0300

    plot num of bad trials also of bad subs.

--- analysis/analysis_code/plotNumBadTrials.m
+++ analysis/analysis_code/plotNumBadTrials.m
@@ -8,14 +8,13 @@ function [] = plotNumBadTrials(traj_name, plt_p, p)
     n_bad_trials = load([p.PROC_DATA_FOLDER '/bad_trials_' p.DAY '_' traj_name '_subs_' subs_string '.mat']);
     reach_n_bad_trials = n_bad_trials.reach_n_bad_trials;
     keyboard_n_bad_trials = n_bad_trials.keyboard_n_bad_trials;
-    good_subs = load([p.PROC_DATA_FOLDER '/good_subs_' p.DAY '_' traj_name '_subs_' subs_string '.mat']);  good_subs = good_subs.good_subs;
     num_reasons = size(reach_n_bad_trials,2);
     reasons = string(replace(reach_n_bad_trials.Properties.VariableNames, '_', ' '));
 
     % Set parameters for plot.
     for i_reason = 1:num_reasons
-        beesdata{:, i_reason*2 - 1} = reach_n_bad_trials{good_subs, i_reason}';
-        beesdata{:, i_reason*2}     = keyboard_n_bad_trials{good_subs, i_reason}';
+        beesdata{:, i_reason*2 - 1} = reach_n_bad_trials{p.SUBS, i_reason}';
+        beesdata{:, i_reason*2}     = keyboard_n_bad_trials{p.SUBS, i_reason}';
     end
     yLabel = 'Number of bad trials';
     XTickLabel = [];
@@ -31,11 +30,10 @@ function [] = plotNumBadTrials(traj_name, plt_p, p)
     font_size = [1, 12];
     groupTick(ticks, labels, dist, font_size)
 
-    ylim([-20 420]);
     % Legend.
     h = [];
-    h(1) = bar(NaN,NaN,'FaceColor',plt_p.exp_2_color);
-    h(2) = bar(NaN,NaN,'FaceColor',plt_p.exp_3_color);
+    h(1) = bar(NaN,NaN,'FaceColor',plt_p.reach_color);
+    h(2) = bar(NaN,NaN,'FaceColor',plt_p.keyboard_color);
     legend(h,'Reach','Keyboard', 'Location','northwest');
     % T-test.
     for i_reason = 1:num_reasons
