7ccdf4c32f33717d5ddc6a85a1c3aaca9d5cc27c
khenheller <khenheller@mail.tau.ac.il>
2022-09-02 06:52:19 +0300

    Correct AUC multiple comparisons

--- analysis/analysis_code/plotTreeBH.m
+++ analysis/analysis_code/plotTreeBH.m
@@ -1,14 +1,15 @@
 function [] = plotTreeBH(plt_p, p)
     % Specify tree structure.
-    parents_vec = [1 1 2 2 3 4 4 4 4 5];
+    parents_vec = [1 1 2 2 3 4 4 4 4 4 5];
     node_names = [{'effect size comparison'},...
         {'reach','keyboard'},...
         {'exploratory','confirmatory','rt'},...
-        {'react','mt','com','tot dist','ra'}];
+        {'react','mt','com','tot dist','auc','ra'}];
     
     % Add p-values to tree.
     keyboard_rt_p_val = load([p.PROC_DATA_FOLDER '/keyboard_rt_p_val_' p.DAY '_subs_' p.SUBS_STRING '.mat']);  keyboard_rt_p_val = keyboard_rt_p_val.p_val;
     tot_dist_p_val = load([p.PROC_DATA_FOLDER '/tot_dist_p_val_' p.DAY '_subs_' p.SUBS_STRING '.mat']);  tot_dist_p_val = tot_dist_p_val.p_val;
+    auc_p_val = load([p.PROC_DATA_FOLDER '/auc_p_val_' p.DAY '_subs_' p.SUBS_STRING '.mat']);  auc_p_val = auc_p_val.p_val;
     com_p_val = load([p.PROC_DATA_FOLDER '/com_p_val_' p.DAY '_subs_' p.SUBS_STRING '.mat']);  com_p_val = com_p_val.p_val;
     ra_p_val = load([p.PROC_DATA_FOLDER '/ra_p_val_' p.DAY '_subs_' p.SUBS_STRING '.mat']);  ra_p_val = ra_p_val.p_val;
     react_mt_rt_p_val = load([p.PROC_DATA_FOLDER '/react_mt_rt_p_val_' p.DAY '_subs_' p.SUBS_STRING '.mat']);  react_mt_rt_p_val = react_mt_rt_p_val.p_val;
@@ -17,7 +18,7 @@ function [] = plotTreeBH(plt_p, p)
     node_p_values = [nan,...
         nan, nan,...
         nan, nan, keyboard_rt_p_val,...
-        react_p_val, mt_p_val, com_p_val, tot_dist_p_val, ra_p_val];
+        react_p_val, mt_p_val, com_p_val, tot_dist_p_val, auc_p_val, ra_p_val];
     
     % Create a a tree.
     g = createtree(parents_vec, node_names, node_p_values);
