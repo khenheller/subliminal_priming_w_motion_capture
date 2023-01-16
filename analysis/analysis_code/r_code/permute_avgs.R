d875679a8acca0c59d2b077e2951a2d6ce0413e7
khenheller <khenheller@mail.tau.ac.il>
2023-01-16 12:59:48 +0200

    R saves p val with correct name

--- analysis/analysis_code/r_code/permute_avgs.R
+++ analysis/analysis_code/r_code/permute_avgs.R
@@ -8,10 +8,11 @@ permute_avgs <- function(p){
     if (!is_normal){
       temp_df <- data.frame(readRDS(paste0(p$PROC_DATA_FOLDER,"/",var_name,"_df.rds")))
       # Remove prefix.
-      new_var_name <- sub('[r,k]_','',var_name)
+      clean_var_name <- sub('[r,k]_','',var_name)
+      new_var_name <- clean_var_name
       # Select var: standardized or not.
       if(p$STNDRD){
-        new_var_name <- paste0(new_var_name,'_stn')
+        new_var_name <- paste0(clean_var_name,'_stn')
       }
       # Permutate
       perm_result = perm.t.test(formula=get(new_var_name)~cond, data=temp_df, paired=TRUE, conf.level=1-alpha)
@@ -19,7 +20,7 @@ permute_avgs <- function(p){
       cat(paste('------',new_var_name,'------\n'))
       print(perm_result)
       # Save results.
-      writeMat(con=paste0(p$PROC_DATA_FOLDER,'/',new_var_name,'_p_val_',p$DAY,'_',p$EXP,'.mat'), p_val=perm_result$perm.p.value, ci=perm_result$perm.conf.int)
+      writeMat(con=paste0(p$PROC_DATA_FOLDER,'/',clean_var_name,'_p_val_',p$DAY,'_',p$EXP,'.mat'), p_val=perm_result$perm.p.value, ci=perm_result$perm.conf.int)
     }
   }
   cat("Permutation testing done.\n")
