7225e02e8cb4c2953a9359ea1745d450f6e278b4
khenheller <khenheller@mail.tau.ac.il>
2023-03-07 14:13:31 +0200

    Count COM with iEP instead of angle

--- analysis/analysis_code/countCom.m
+++ analysis/analysis_code/countCom.m
@@ -1,6 +1,6 @@
 % Counts the number of Changes Of Minf (COM) in each trial.
-% COM happens when the subject changes his end goal, this is
-% reflected by a change in the heading angle's sign.
+% COM happens when the subject changes his implied endpoint from
+% one side of the screen to the other.
 function data_table = countCom(traj_table, data_table, p)
     traj_len = load([p.PROC_DATA_FOLDER '/trim_len.mat']);  traj_len = traj_len.trim_len;
     assert(any(contains(traj_table.Properties.VariableNames, 'head_angle')), "Run heading angle section before COM section");
@@ -11,12 +11,12 @@ function data_table = countCom(traj_table, data_table, p)
     com = NaN(p.NUM_TRIALS,1);
 
     % Reshape to convinient format.
-    angles = traj_table{:,'head_angle'};
-    angles_mat = reshape(angles, traj_len, p.NUM_TRIALS);
+    iep = traj_table{:,'iep'};
+    iep_mat = reshape(iep, traj_len, p.NUM_TRIALS); % X values of iEP.
 
     for iTrial = 1:height(data_table)
         % Find all negative angles.
-        neg_angle = angles_mat(:, iTrial) < 0;
+        neg_angle = iep_mat(:, iTrial) < 0;
         neg_angle(1) = neg_angle(2); % There is no angle at first sample, so there is no change from first to second sample.
         % Find changes in sign.
         num_com = length(find(diff(neg_angle)));
