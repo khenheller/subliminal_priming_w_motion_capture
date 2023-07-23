c8c0c5a47091012e5bfdc8f5dda3cbd8bb07472a
Khenheller <khenheller@mail.tau.ac.il>
2023-07-23 13:53:24 +0300

    Changes from Guy's Ubuntu

--- analysis/analysis_code/calcReachArea.m
+++ analysis/analysis_code/calcReachArea.m
@@ -9,8 +9,8 @@ function [r_a] = calcReachArea(left, right, p)
     % Convert to micro-meters.
     left(:,3) = left(:,3) * p.SCREEN_DIST * 100;
     right(:,3) = right(:,3) * p.SCREEN_DIST * 100;
-    % If traj not normalized to Z, loops can occur.
-    % To avoid, replaced Z with time (can't reverse direction [unless a timemachine is at hand]).
+    % If traj not normalized to Z, loops can occur in traj.
+    % To avoid that, replaced Z with time (time can't reverse direction [unless a timemachine is at hand]).
     if ~p.NORM_TRAJ
         time_series = (1 : p.MAX_CAP_LENGTH) * p.SAMPLE_RATE_SEC; % Array with timing of each sample.
         last_sample_left = find(~isnan(left(:,3)), 1, 'last');
